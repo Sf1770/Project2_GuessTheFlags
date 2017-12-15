83d11746f57cb32b8711ea96babe939c3a4d95bd
Sf1770 <sf1770@nyu.edu>
2017-12-15 10:36:48 -0500

    Initial Commit

---
+++ Project2_Guess_The_Flag/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Project2_Guess_The_Flag
+//
+//  Created by Sabrina Fletcher on 12/15/17.
+//  Copyright © 2017 Sabrina Fletcher. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
